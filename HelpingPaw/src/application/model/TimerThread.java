472120bce48ba61e5c9577780c7800bef2e0a9e6
Andrea Mendez <andreacelestem11@gmail.com>
2022-11-22 15:39:19 -0600

    updated TimerController and thread, pause and done functionality added!

--- HelpingPaw/src/application/model/TimerThread.java
+++ HelpingPaw/src/application/model/TimerThread.java
@@ -1,22 +1,29 @@
 package application.model;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.concurrent.TimeUnit;
-
+import java.time.temporal.ChronoUnit;
 import com.google.common.base.Stopwatch;
 
-public class TimerThread extends Thread {
+public class TimerThread extends Thread{
 	Stopwatch stopwatch = Stopwatch.createUnstarted();
 	long duration = 0;
 	long millis = TimeUnit.MILLISECONDS.convert(1L,TimeUnit.SECONDS);
 	long min;
 	long currMin = 200;
 	long currSec = 200;
+	Duration total;
 	public TimerThread(long m) {
 		this.min = m;
+		currMin = min;
+		total = Duration.ofMinutes(min);
 	}
 	public TimerThread(long m , long s) {
 		this.min = m;
+		this.currMin = min;
 		this.currSec = s;
+		total = Duration.ofMinutes(min+1);
 	}
 	public void stopTimer() {
 		stopwatch.reset();
@@ -24,33 +31,39 @@ public class TimerThread extends Thread {
 
 	public long[] pauseTimer() {
 		stopwatch.stop();
-		long totalSec = stopwatch.elapsed(TimeUnit.SECONDS);
+		Duration elapsed = Duration.ofSeconds(stopwatch.elapsed(TimeUnit.SECONDS));
+		stopwatch.reset();
+		long totalSec = total.getSeconds()-elapsed.getSeconds();
 		currMin = totalSec/60;
 		currSec = totalSec%60;
-		long[] dur = {currMin,currSec};
-		stopwatch.reset();
+		long[] dur = {currMin,currSec-2};
 		return dur;
 	}
+
 	public void run() {
+		stopwatch.start();
 		if(currSec == 200) {
-			currMin = min-1;
 			currSec = 60;
-			stopwatch.start();
-			application.controller.TimerController.updateTime(min+":00");
+			application.controller.TimerController.updateTime(currMin--+":00");
 		}
 		else {
-			stopwatch.start();
 			if(currSec>10) {
-				application.controller.TimerController.updateTime(min+":"+currSec);
+				System.out.println(currMin + ":" + currSec);
+				application.controller.TimerController.updateTime(currMin+":"+currSec);
 			}
 			else {
-				application.controller.TimerController.updateTime(min+":0"+currSec);
+				application.controller.TimerController.updateTime(currMin+":0"+currSec);
 			}
 		}
 		while(duration < min) {
+			if(!stopwatch.isRunning()) {
+				System.out.println("Pausing timer");
+				break;
+			}
 			try {
-				Thread.sleep(millis);
+				TimeUnit.SECONDS.sleep(1);
 			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			if(currSec-1==0 && currMin>0) {
@@ -68,6 +81,5 @@ public class TimerThread extends Thread {
 
 			duration = stopwatch.elapsed(TimeUnit.MINUTES);
 		}
-		stopwatch.stop(); // optional
 	}
 }
