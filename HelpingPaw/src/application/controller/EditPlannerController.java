4b6af014aa209ad9f5a96ffe7a7b5b057e585e19
Laoro <Laoro@LaorosWorld.utsarr.net>
2022-11-22 14:56:28 -0600

    Add Action to btHome

--- HelpingPaw/src/application/controller/EditPlannerController.java
+++ HelpingPaw/src/application/controller/EditPlannerController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,7 +10,10 @@ import application.model.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
@@ -17,6 +21,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 
 
@@ -55,7 +60,6 @@ public class EditPlannerController implements EventHandler<ActionEvent>, Initial
 		rectangleEP.setFill(Main.user.color);
 		String col = User.colorToString(Main.user.color);
 		
-		btBack.setStyle("-fx-background-color: " + col);
 		btDelete.setStyle("-fx-background-color: " + col);
 		btAdd.setStyle("-fx-background-color: " + col);
 		
@@ -66,6 +70,16 @@ public class EditPlannerController implements EventHandler<ActionEvent>, Initial
 
 	@Override
 	public void handle(ActionEvent event) {
-		
+		Button btChoose = (Button) event.getSource();
+		if(btChoose == btBack) {
+			try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/PlannerView.fxml"));
+				Stage window = (Stage)btBack.getScene().getWindow();
+				window.setScene(new Scene(root, 800, 800));
+			
+			} catch(IOException e) {
+			e.printStackTrace();				
+			}
+		}
 	}
 }
